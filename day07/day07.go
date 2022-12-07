eb1bdca0c7d8ec1d323f7063715fe7247853f87b
Chris <chris.finlow@gmail.com>
2022-12-07 08:55:39 +0000

    progress

--- day07/day07.go
+++ day07/day07.go
@@ -1,12 +1,70 @@
 package day07
 
-import "github.com/christheoreo/advent-of-code-2022/internal/filereader"
+import (
+	"fmt"
+	"strings"
 
-func SolveFirst(filename string) string {
-	data, _ := filereader.ReadFileToStringArray(filename)
-	return ""
+	"github.com/christheoreo/advent-of-code-2022/internal/filereader"
+)
+
+type Folder struct {
+	Folders []Folder
+	Size    int
 }
-func SolveSecond(filename string) string {
+
+// func SolveFirst(filename string) int {
+// 	data, _ := filereader.ReadFileToStringArray(filename)
+// 	path := make([]string, 0)
+// 	for _, val := range data {
+// 		if val == "$ cd /" {
+// 			continue
+// 		}
+// 		if strings.Contains(val, "$ cd") {
+// 			slice := strings.Split(val, " ")
+// 			folderName := slice[len(slice)-1]
+// 			if folderName == ".." {
+// 				path = path[:len(path)-1]
+// 			} else {
+// 				path = append(path, folderName)
+// 			}
+
+// 			fmt.Println(path)
+// 		}
+// 	}
+// 	fmt.Println(folders)
+// 	return 0
+// }
+
+func SolveFirst(filename string) int {
 	data, _ := filereader.ReadFileToStringArray(filename)
-	return ""
+	folders := make([]Folder, 0)
+	folder := Folder{
+		Folders: []Folder{},
+		Size:    0,
+	}
+	for _, val := range data {
+		if val == "$ cd /" {
+			continue
+		}
+		if strings.Contains(val, "$ cd") {
+			slice := strings.Split(val, " ")
+			folderName := slice[len(slice)-1]
+
+			newFolder := Folder{
+				Folders: []Folder{},
+				Size:    0,
+			}
+
+			
+			continue
+		}
+		fmt.Println(folders)
+		return 0
+	}
+func SolveSecond(filename string) int {
+	data, _ := filereader.ReadFileToStringArray(filename)
+	for _, val := range data {
+		fmt.Println(val)
+	}
+	return 0
 }
--- day07/day07_test.go
+++ day07/day07_test.go
@@ -1,7 +1,6 @@
 package day07
 
 import (
-	"fmt"
 	"testing"
 )
 
@@ -9,26 +8,26 @@ var testFile string = "problem-7-example.txt"
 var realFile string = "problem-7.txt"
 
 func TestSolveFirst(t *testing.T) {
-	expected := ""
+	expected := 95437
 	actual := SolveFirst(testFile)
 	if expected != actual {
-		t.Errorf("Expected %s but got %s", expected, actual)
+		t.Errorf("Expected %d but got %d", expected, actual)
 		t.Fail()
 	}
-	answer := SolveFirst(realFile)
-	fmt.Printf("\n-----\n --ANSWER Day 07 part 1 = %s \n-----\n", answer)
+	// answer := SolveFirst(realFile)
+	// fmt.Printf("\n-----\n --ANSWER Day 07 part 1 = %s \n-----\n", answer)
 }
 
-func TestSolveSecond(t *testing.T) {
-	expected := ""
-	actual := SolveSecond(testFile)
-	if expected != actual {
-		t.Errorf("Expected %s but got %s", expected, actual)
-		t.Fail()
-	}
-	answer := SolveSecond(realFile)
-	fmt.Printf("\n-----\n --ANSWER Day 07 part 2 = %s \n-----\n", answer)
-}
+// func TestSolveSecond(t *testing.T) {
+// 	expected := 0
+// 	actual := SolveSecond(testFile)
+// 	if expected != actual {
+// 		t.Errorf("Expected %s but got %s", expected, actual)
+// 		t.Fail()
+// 	}
+// 	answer := SolveSecond(realFile)
+// 	fmt.Printf("\n-----\n --ANSWER Day 07 part 2 = %s \n-----\n", answer)
+// }
 
 func BenchmarkSolveFirst(b *testing.B) {
 	for n := 0; n < b.N; n++ {
