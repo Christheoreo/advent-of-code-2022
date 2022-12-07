f6bf76aaf7476f2df71f19770ad2d019599d9438
Chris Finlow <chris.finlow@gmail.com>
2022-12-07 12:04:57 +0000

    Prepared for day 08

---
+++ day08/day08.go
@@ -0,0 +1,10 @@
+package day08
+
+func SolveFirst(filename string) int {
+	// data, _ := filereader.ReadFileToStringArray(filename)
+	return 0
+}
+func SolveSecond(filename string) int {
+	// data, _ := filereader.ReadFileToStringArray(filename)
+	return 0
+}
---
+++ day08/day08_test.go
@@ -0,0 +1,42 @@
+package day08
+
+import (
+	"fmt"
+	"testing"
+)
+
+var testFile string = "problem-8-example.txt"
+var realFile string = "problem-8.txt"
+
+func TestSolveFirst(t *testing.T) {
+	expected := 0
+	actual := SolveFirst(testFile)
+	if expected != actual {
+		t.Errorf("Expected %d but got %d", expected, actual)
+		t.Fail()
+	}
+	answer := SolveFirst(realFile)
+	fmt.Printf("\n-----\n --ANSWER Day 08 part 1 = %d \n-----\n", answer)
+}
+
+func TestSolveSecond(t *testing.T) {
+	expected := 0
+	actual := SolveSecond(testFile)
+	if expected != actual {
+		t.Errorf("Expected %d but got %d", expected, actual)
+		t.Fail()
+	}
+	answer := SolveSecond(realFile)
+	fmt.Printf("\n-----\n --ANSWER Day 08 part 2 = %d \n-----\n", answer)
+}
+
+func BenchmarkSolveFirst(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		SolveFirst(realFile)
+	}
+}
+func BenchmarkSolveSecond(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		SolveSecond(realFile)
+	}
+}
