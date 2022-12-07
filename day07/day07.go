0b53739a9279ddd2da72c3adb6da731494d836e2
Chris Finlow <chris.finlow@gmail.com>
2022-12-07 10:30:01 +0000

    working solution!

--- day07/day07.go
+++ day07/day07.go
@@ -2,69 +2,228 @@ package day07
 
 import (
 	"fmt"
+	"strconv"
 	"strings"
 
 	"github.com/christheoreo/advent-of-code-2022/internal/filereader"
 )
 
 type Folder struct {
-	Folders []Folder
-	Size    int
+	Children []*Folder
+	Parent   *Folder
+	Size     int
+	Path     string
 }
 
-// func SolveFirst(filename string) int {
-// 	data, _ := filereader.ReadFileToStringArray(filename)
-// 	path := make([]string, 0)
-// 	for _, val := range data {
-// 		if val == "$ cd /" {
-// 			continue
-// 		}
-// 		if strings.Contains(val, "$ cd") {
-// 			slice := strings.Split(val, " ")
-// 			folderName := slice[len(slice)-1]
-// 			if folderName == ".." {
-// 				path = path[:len(path)-1]
-// 			} else {
-// 				path = append(path, folderName)
-// 			}
+var runningTotal int
 
-// 			fmt.Println(path)
-// 		}
-// 	}
-// 	fmt.Println(folders)
-// 	return 0
-// }
+var smallestFolder int
 
 func SolveFirst(filename string) int {
+	runningTotal = 0
 	data, _ := filereader.ReadFileToStringArray(filename)
-	folders := make([]Folder, 0)
-	folder := Folder{
-		Folders: []Folder{},
-		Size:    0,
-	}
+	var currentFolder *Folder
 	for _, val := range data {
-		if val == "$ cd /" {
-			continue
-		}
+
 		if strings.Contains(val, "$ cd") {
 			slice := strings.Split(val, " ")
 			folderName := slice[len(slice)-1]
-
-			newFolder := Folder{
-				Folders: []Folder{},
-				Size:    0,
+			if currentFolder == nil {
+				fmt.Printf("Creating new folder called %s\n", folderName)
+				currentFolder = &Folder{
+					Children: make([]*Folder, 0),
+					Size:     0,
+					Parent:   nil,
+					Path:     folderName,
+				}
+			} else if val[5:] == ".." {
+				fmt.Printf("Gone up a directory - currently in %s , goingto %s \n", currentFolder.Path, currentFolder.Parent.Path)
+				currentFolder = currentFolder.Parent
+			} else {
+				for _, folder := range currentFolder.Children {
+					if folder.Path == val[5:] {
+						fmt.Printf("Gone down a directory - currently in %s , going down to %s \n", currentFolder.Path, folder.Path)
+						currentFolder = folder
+						break
+					}
+				}
 			}
-
-			
 			continue
 		}
-		fmt.Println(folders)
-		return 0
+
+		if strings.Contains(val, "dir") {
+			slice := strings.Split(val, " ")
+			folderName := slice[len(slice)-1]
+			newFolder := &Folder{
+				Children: make([]*Folder, 0),
+				Size:     0,
+				Parent:   currentFolder,
+				Path:     folderName,
+			}
+			fmt.Printf("Creating a sub folder in %s called %s\n", currentFolder.Path, newFolder.Path)
+			currentFolder.Children = append(currentFolder.Children, newFolder)
+			continue
+		}
+
+		if strings.Contains(val, "$ ls") {
+			continue
+		}
+
+		numString := strings.Split(val, " ")[0]
+
+		num, err := strconv.Atoi(numString)
+
+		if err != nil {
+			panic(err)
+		}
+
+		// fmt.Println(num)
+
+		currentFolder.Size += num
 	}
+
+	for currentFolder.Path != "/" {
+		currentFolder = currentFolder.Parent
+	}
+	recursivelyPrint(currentFolder)
+	getSize(currentFolder)
+	// for currentFolder.Parent != nil {
+	// 	currentFolder = currentFolder.Parent
+	// }
+
+	// fmt.Println(len(currentFolder.Children))
+	// fmt.Println(len(currentFolder.Path))
+	return runningTotal
+}
+
+func recursivelyPrint(folder *Folder) {
+	fmt.Printf("Folder %s has a size of %d\n", folder.Path, folder.Size)
+	for _, val := range folder.Children {
+		recursivelyPrint(val)
+	}
+}
+
+func getSize(folder *Folder) int {
+	var size int = folder.Size
+	for _, val := range folder.Children {
+		s := getSize(val)
+		size += s
+	}
+
+	fmt.Printf("Folder %s has a size of %d\n", folder.Path, size)
+	if size <= 100000 {
+		fmt.Printf("Adding %d to the total (%d) to make %d\n", size, runningTotal, runningTotal+size)
+		runningTotal += size
+	}
+	return size
+}
 func SolveSecond(filename string) int {
+
+	smallestFolder = 0
 	data, _ := filereader.ReadFileToStringArray(filename)
+	var currentFolder *Folder
 	for _, val := range data {
-		fmt.Println(val)
+
+		if strings.Contains(val, "$ cd") {
+			slice := strings.Split(val, " ")
+			folderName := slice[len(slice)-1]
+			if currentFolder == nil {
+				fmt.Printf("Creating new folder called %s\n", folderName)
+				currentFolder = &Folder{
+					Children: make([]*Folder, 0),
+					Size:     0,
+					Parent:   nil,
+					Path:     folderName,
+				}
+			} else if val[5:] == ".." {
+				fmt.Printf("Gone up a directory - currently in %s , goingto %s \n", currentFolder.Path, currentFolder.Parent.Path)
+				currentFolder = currentFolder.Parent
+			} else {
+				for _, folder := range currentFolder.Children {
+					if folder.Path == val[5:] {
+						fmt.Printf("Gone down a directory - currently in %s , going down to %s \n", currentFolder.Path, folder.Path)
+						currentFolder = folder
+						break
+					}
+				}
+			}
+			continue
+		}
+
+		if strings.Contains(val, "dir") {
+			slice := strings.Split(val, " ")
+			folderName := slice[len(slice)-1]
+			newFolder := &Folder{
+				Children: make([]*Folder, 0),
+				Size:     0,
+				Parent:   currentFolder,
+				Path:     folderName,
+			}
+			fmt.Printf("Creating a sub folder in %s called %s\n", currentFolder.Path, newFolder.Path)
+			currentFolder.Children = append(currentFolder.Children, newFolder)
+			continue
+		}
+
+		if strings.Contains(val, "$ ls") {
+			continue
+		}
+
+		numString := strings.Split(val, " ")[0]
+
+		num, err := strconv.Atoi(numString)
+
+		if err != nil {
+			panic(err)
+		}
+
+		// fmt.Println(num)
+
+		currentFolder.Size += num
 	}
-	return 0
+
+	for currentFolder.Path != "/" {
+		currentFolder = currentFolder.Parent
+	}
+	recursivelyPrint(currentFolder)
+
+	const capacity = 70000000
+	const spaceNeeded = 30000000
+	totalSize := getSizeOfThisFolder(currentFolder)
+
+	deleteSize := spaceNeeded - (capacity - totalSize)
+	getSizeTwo(currentFolder, deleteSize)
+
+	fmt.Printf("capacity - total size = %d\n", capacity-totalSize)
+	fmt.Printf("delete size = %d\n", deleteSize)
+
+	// fmt.Printf("Total size = %d\n")
+	// for currentFolder.Parent != nil {
+	// 	currentFolder = currentFolder.Parent
+	// }
+
+	// fmt.Println(len(currentFolder.Children))
+	// fmt.Println(len(currentFolder.Path))
+	return smallestFolder
+}
+func getSizeOfThisFolder(folder *Folder) int {
+	var size int = folder.Size
+	for _, val := range folder.Children {
+		s := getSize(val)
+		size += s
+	}
+	return size
+}
+func getSizeTwo(folder *Folder, sizeNeeded int) int {
+	var size int = folder.Size
+	for _, val := range folder.Children {
+		s := getSizeTwo(val, sizeNeeded)
+		size += s
+	}
+
+	fmt.Printf("Folder %s has a size of %d\n", folder.Path, size)
+	if size > sizeNeeded && (smallestFolder == 0 || size < smallestFolder) {
+		fmt.Printf("Adding %d to the total (%d) to make %d\n", size, runningTotal, runningTotal+size)
+		smallestFolder = size
+	}
+	return size
 }
--- day07/day07_test.go
+++ day07/day07_test.go
@@ -1,6 +1,7 @@
 package day07
 
 import (
+	"fmt"
 	"testing"
 )
 
@@ -14,20 +15,20 @@ func TestSolveFirst(t *testing.T) {
 		t.Errorf("Expected %d but got %d", expected, actual)
 		t.Fail()
 	}
-	// answer := SolveFirst(realFile)
-	// fmt.Printf("\n-----\n --ANSWER Day 07 part 1 = %s \n-----\n", answer)
+	answer := SolveFirst(realFile)
+	fmt.Printf("\n-----\n --ANSWER Day 07 part 1 = %d \n-----\n", answer)
 }
 
-// func TestSolveSecond(t *testing.T) {
-// 	expected := 0
-// 	actual := SolveSecond(testFile)
-// 	if expected != actual {
-// 		t.Errorf("Expected %s but got %s", expected, actual)
-// 		t.Fail()
-// 	}
-// 	answer := SolveSecond(realFile)
-// 	fmt.Printf("\n-----\n --ANSWER Day 07 part 2 = %s \n-----\n", answer)
-// }
+func TestSolveSecond(t *testing.T) {
+	expected := 24933642
+	actual := SolveSecond(testFile)
+	if expected != actual {
+		t.Errorf("Expected %d but got %d", expected, actual)
+		t.Fail()
+	}
+	answer := SolveSecond(realFile)
+	fmt.Printf("\n-----\n --ANSWER Day 07 part 2 = %d \n-----\n", answer)
+}
 
 func BenchmarkSolveFirst(b *testing.B) {
 	for n := 0; n < b.N; n++ {
