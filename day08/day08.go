4ef882bbdcc287cde578b851a582cef5bf2f8b6b
Chris Finlow <chris.finlow@gmail.com>
2022-12-08 13:48:56 +0000

    improved speed

--- day08/day08.go
+++ day08/day08.go
@@ -62,6 +62,7 @@ func SolveSecond(filename string) int {
 	highscore := 0
 	rowsTotal := len(data)
 	rows := make([][]int, rowsTotal)
+	rowLength := len(data[0])
 
 	// setup the ints
 	for rowIndex, line := range data {
@@ -75,37 +76,16 @@ func SolveSecond(filename string) int {
 		row := rows[rowIndex]
 		for i := 1; i < len(row)-1; i++ {
 			tree := row[i]
-
-			rightSide := row[i+1:]
-			leftSide := make([]int, 0)
-			above := make([]int, 0)
-			below := make([]int, 0)
-
-			for rI := rowIndex - 1; rI >= 0; rI-- {
-				above = append(above, rows[rI][i])
+			above := make([]int, rowIndex+1)
+			below := make([]int, rowsTotal-1-rowIndex+1)
+			for k, l := rowIndex, 0; k >= 0; k, l = k-1, l+1 {
+				above[l] = rows[k][i]
 			}
-			for rI := rowIndex + 1; rI <= rowsTotal-1; rI++ {
-				below = append(below, rows[rI][i])
+			for k, l := rowIndex, 0; k < rowsTotal; k, l = k+1, l+1 {
+				below[l] = rows[k][i]
 			}
-			for cI := i - 1; cI >= 0; cI-- {
-				leftSide = append(leftSide, row[cI])
-			}
-
-			numbers := [4]int{0, 0, 0, 0}
-			slices := [4][]int{rightSide, leftSide, above, below}
-			for sliceI, slice := range slices {
-				n := 0
-				for _, num := range slice {
-					n++
-					if tree > num {
-						continue
-					}
-					break
-				}
-				numbers[sliceI] = n
-			}
-
-			score := numbers[0] * numbers[1] * numbers[2] * numbers[3]
+			a, b, c, d := findLeft(row, i, tree), findRight(row, i, rowLength, tree), findNext(above, 0, tree), findNext(below, 0, tree)
+			score := a * b * c * d
 			if score > highscore {
 				highscore = score
 			}
@@ -113,4 +93,45 @@ func SolveSecond(filename string) int {
 	}
 
 	return highscore
+}
+
+func findRight(row []int, c int, rowLength int, tree int) int {
+	count := 1
+	// is there any to the right?
+	if c > rowLength-2 {
+		return 0
+	}
+	c += 1
+	newTree := row[c]
+	if tree > newTree {
+		count += findRight(row, c, rowLength, tree)
+	}
+	return count
+}
+
+func findLeft(row []int, c int, tree int) int {
+	count := 1
+	// is there any to the right?
+	if c < 1 {
+		return 0
+	}
+	c -= 1
+	newTree := row[c]
+	if tree > newTree {
+		count += findLeft(row, c, tree)
+	}
+	return count
+}
+
+func findNext(col []int, r int, tree int) int {
+	count := 1
+	if r > len(col)-2 {
+		return 0
+	}
+	r += 1
+	newTree := col[r]
+	if tree > newTree {
+		count += findNext(col, r, tree)
+	}
+	return count
 }
