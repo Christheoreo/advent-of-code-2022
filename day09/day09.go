ff70e0b85e1332653941719dce77acce8b3b0788
Chris <chris.finlow@gmail.com>
2022-12-09 07:11:04 +0000

    Populated the instructions slice

--- day09/day09.go
+++ day09/day09.go
@@ -1,7 +1,43 @@
 package day09
 
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/christheoreo/advent-of-code-2022/internal/filereader"
+)
+
+type Direction int64
+
+const (
+	Up Direction = iota
+	Down
+	Left
+	Right
+)
+
+type Instruction struct {
+	// Direction Direction
+	Direction string
+	Count     int
+}
+
 func SolveFirst(filename string) int {
-	// data, _ := filereader.ReadFileToStringArray(filename)
+	data, _ := filereader.ReadFileToStringArray(filename)
+	instructions := make([]Instruction, len(data))
+
+	for index, val := range data {
+		num, _ := strconv.Atoi(val[2:])
+		instruction := Instruction{
+			Direction: string(val[0]),
+			Count:     num,
+		}
+		instructions[index] = instruction
+	}
+
+	for _, instuction := range instructions {
+		fmt.Printf("Direction = %s and count = %d\n", instuction.Direction, instuction.Count)
+	}
 	return 0
 }
 func SolveSecond(filename string) int {
