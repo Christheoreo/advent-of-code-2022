a7e208df14bafdce27df69a5bf8b84c3acd9b5fe
Chris <chris.finlow@gmail.com>
2022-12-09 19:19:39 +0000

    Perpared for day 10

---
+++ day10/day10.go
@@ -0,0 +1,16 @@
+package day10
+
+type Instruction struct {
+	Direction string
+	Count     int
+}
+
+func SolveFirst(filename string) int {
+	// data, _ := filereader.ReadFileToStringArray(filename)
+	return 0
+}
+
+func SolveSecond(filename string) int {
+	// data, _ := filereader.ReadFileToStringArray(filename)
+	return 0
+}
