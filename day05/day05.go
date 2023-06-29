e15f5369ff9e20d62fde5cd8305e6b745a17826c
Chris <chris.finlow@gmail.com>
2023-06-29 13:15:46 +0100

    part 2 done

--- day05/day05.go
+++ day05/day05.go
@@ -6,52 +6,13 @@ import (
 	"strings"
 )
 
-// func SolveFirst(data string) string {
-// 	// Find the first line including " 1"
-// 	lines := strings.Split(data, "\n")
-// 	numberIndex := 0
-// 	for lineIndex, line := range lines {
-// 		if strings.Contains(line, " 1") {
-// 			numberIndex = lineIndex
-// 			break
-// 		}
-// 	}
-
-// 	numberLine := lines[numberIndex]
-
-// 	x := strings.TrimSpace(numberLine)
-// 	columnCount := len(strings.Split(x, " "))
-
-// 	return ""
-// }
-
 const emptyS = byte(32)
 
-func SolveFirst(data string) string {
-	// Find the first line including " 1"
-	// mapping := map[int][]byte{}
+func sortColumns(data []string) [][]byte {
 	mapping := make([][]byte, 0)
-	tippingPoint := strings.Index(data, "\nmove")
-	// lastRow := tippingPoint - 2
-
-	numbers := data[:tippingPoint-1]
-	// fmt.Println((numbers))
-	other := data[tippingPoint+1:]
-	// columnCount, _ := strconv.Atoi(string(data[tippingPoint-3]))
-	// fmt.Println(columnCount)
-
-	// 4 * col - 1 = index
-
-	// Start at index 1, then add 4 to get col value until we run out of string
-	numbersSplit := strings.Split(numbers, "\n")
-
-	// fmt.Println(numbers)
-	// fmt.Println(numbersSplit)
-	// for _, val := range numbersSplit {
-	for xx := len(numbersSplit) - 2; xx >= 0; xx-- {
-		val := numbersSplit[xx]
+	for xx := len(data) - 2; xx >= 0; xx-- {
+		val := data[xx]
 		l := len(val)
-		// fmt.Printf("Val = %s and l = %d\n", val, l)
 
 		for i, j := 1, 1; i <= l; i, j = i+4, j+1 {
 
@@ -62,32 +23,32 @@ func SolveFirst(data string) string {
 			}
 
 			ok := len(mapping) >= j
-			// _, ok := mapping[j]
 
 			if !ok {
-				// fmt.Printf("here - %d is greater than %d\n", j, len(mapping))
 				for i := len(mapping); i <= j-1; i++ {
 					mapping = append(mapping, make([]byte, 0))
 				}
-				// mapping[j] = make([]byte, 0)
 			}
-			// fmt.Println(len(mapping))
-			// fmt.Println(j)
 			mapping[j-1] = append(mapping[j-1], r)
 		}
 	}
+	return mapping
+}
 
-	for col, v := range mapping {
-		fmt.Printf("Length of %d is %d\n", col, len(v))
-		for index, x := range v {
-			fmt.Printf("Col %d index = %d = %s\n", col, index, string(x))
-		}
-	}
+func SolveFirst(data string) string {
+	tippingPoint := strings.Index(data, "\nmove")
 
-	for _, val := range strings.Split(other, "\n") {
+	columnsData := data[:tippingPoint-1]
+	instructionsData := data[tippingPoint+1:]
+
+	// Start at index 1, then add 4 to get col value until we run out of string
+	numbersSplit := strings.Split(columnsData, "\n")
+
+	mapping := sortColumns(numbersSplit)
+
+	for _, val := range strings.Split(instructionsData, "\n") {
 		s := strings.Split(val, " ")
 
-		// move, from, to := 0,0,0
 		move, _ := strconv.Atoi(s[1])
 		from, _ := strconv.Atoi(s[3])
 		to, _ := strconv.Atoi(s[5])
@@ -100,17 +61,7 @@ func SolveFirst(data string) string {
 			lastEle := mapping[f][len(mapping[f])-1]
 			mapping[f] = mapping[f][:len(mapping[f])-1]
 			mapping[t] = append(mapping[t], lastEle)
-
-			// lastEle := mapping[from][0]
-			// if len(mapping[from]) == 1 {
-			// 	mapping[from] = make([]byte, 0)
-			// } else {
-			// 	mapping[from] = mapping[from][1:]
-			// }
-			// mapping[to] = append([]byte{lastEle}, mapping[to]...)
 		}
-
-		// fmt.Printf("%s - %s - %s\n", s[1], s[3], s[5])
 	}
 
 	answer := ""
@@ -118,16 +69,41 @@ func SolveFirst(data string) string {
 		answer += string(val[len(val)-1])
 	}
 
-	// for k := 1; k <= len(mapping); k++ {
-	// 	val := mapping[k]
-	// 	answer += string(val[len(val)-1])
-	// }
-
-	// fmt.Println(other)
-
 	return answer
 }
 
 func SolveSecond(data string) string {
-	return ""
+	tippingPoint := strings.Index(data, "\nmove")
+
+	columnsData := data[:tippingPoint-1]
+	instructionsData := data[tippingPoint+1:]
+
+	// Start at index 1, then add 4 to get col value until we run out of string
+	numbersSplit := strings.Split(columnsData, "\n")
+
+	mapping := sortColumns(numbersSplit)
+
+	for _, val := range strings.Split(instructionsData, "\n") {
+		s := strings.Split(val, " ")
+
+		move, _ := strconv.Atoi(s[1])
+		from, _ := strconv.Atoi(s[3])
+		to, _ := strconv.Atoi(s[5])
+
+		m := move
+		f := from - 1
+		t := to - 1
+		formula := len(mapping[f]) - m
+		toMove := mapping[f][formula:]
+		mapping[f] = mapping[f][:formula]
+		mapping[t] = append(mapping[t], toMove...)
+
+	}
+
+	answer := ""
+	for _, val := range mapping {
+		answer += string(val[len(val)-1])
+	}
+
+	return answer
 }
--- day05/day05_test.go
+++ day05/day05_test.go
@@ -12,9 +12,23 @@ var data string
 //go:embed example.txt
 var testData string
 
-func TestSolveFirst(t *testing.T) {
-	expected := "CMZ"
-	actual := SolveFirst(testData)
+// func TestSolveFirst(t *testing.T) {
+// 	expected := "CMZ"
+// 	actual := SolveFirst(testData)
+
+// 	if expected != actual {
+// 		t.Errorf("Expected %s but got %s", expected, actual)
+// 		t.Fail()
+// 		return
+// 	}
+
+// 	answer := SolveFirst(data)
+// 	fmt.Printf("ANSWER Day 05 part 1 = %s\n", answer)
+// }
+
+func TestSolveSecond(t *testing.T) {
+	expected := "MCD"
+	actual := SolveSecond(testData)
 
 	if expected != actual {
 		t.Errorf("Expected %s but got %s", expected, actual)
@@ -22,22 +36,9 @@ func TestSolveFirst(t *testing.T) {
 		return
 	}
 
-	answer := SolveFirst(data)
-	fmt.Printf("ANSWER Day 05 part 1 = %s\n", answer)
+	answer := SolveSecond(data)
+	fmt.Printf("ANSWER Day 05 part 2 = %s\n", answer)
 }
-
-// func TestSolveSecond(t *testing.T) {
-// 	expected := 4
-// 	actual := SolveSecond(testData)
-
-// 	if expected != actual {
-// 		t.Errorf("Expected %d but got %d", expected, actual)
-// 		t.Fail()
-// 	}
-
-// 	answer := SolveSecond(data)
-// 	fmt.Printf("ANSWER Day 04 part 2 = %d\n", answer)
-// }
 
 // func BenchmarkSolveFirst(b *testing.B) {
 // 	for n := 0; n < b.N; n++ {
